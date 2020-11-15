2e658150b4697c02419a29336e66825b8459c5ff
Sewmina Dilshan <sewmina7@gmail.com>
2020-11-16 01:57:57 +0530

    Blog is functional

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const e = require("express");
 
 const app = express();
 
 const titles = [];
 const posts = [];
 
+let errors = [];
+let success = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 let options = {
-  title: "Day 1",
+  titles: titles,
+  posts: posts,
+  errors: errors,
+  success: success,
   compose: "0",
 };
 app.get("/", function (req, res) {
   res.render("home", options);
+
+  //success = [];
+  // errors = [];
 });
 
 app.post("/compose", function (req, res) {
   res.render("home", {
-    title: "Day 1",
+    titles: titles,
+    posts: posts,
+    errors: errors,
+    success: success,
     compose: "1",
   });
+  //success = [];
+  // errors = [];
+});
+
+app.post("/composed", function (req, res) {
+  console.log(req.body);
+  if (req.body.button === "composed") {
+    titles.push(req.body.title);
+    posts.push(req.body.post);
+    // success.push("New Blog post created successfully");
+  } else {
+    //  errors.push("Blog post compose cancelled !");
+  }
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function () {
---
+++ public/index.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+  $(".alert").fadeOut("slow");
+}, 2000);
